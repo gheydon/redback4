8ce1e6c9b9a82d20144a7c270153424bf9079d5f
Gordon Heydon <gordon@heydon.com.au>
2013-05-21 14:07:23 +1000

    Add RedBack 4 Classes

---
+++ lib/RocketSoftware/u2/RedBack/Gateway/socket.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Implements the old Socket connection as an alias of RedBack4
+ */
+namespace RocketSoftware\u2\RedBack\Gateway;
+
+use RocketSoftware\u2\RedBack\Gateway\RedBack4;
+
+class Socket extends RedBack4 {
+  
+}
